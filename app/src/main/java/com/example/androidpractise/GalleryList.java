92b13d751f03c4b8ec2eb63e1733c79a8cfa2a96
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2019-03-04 16:01:10 +0545

    first commit

---
+++ app/src/main/java/com/example/androidpractise/GalleryList.java
@@ -0,0 +1,39 @@
+package com.example.androidpractise;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class GalleryList extends ListFragment implements AdapterView.OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_gallery_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),  R.array.templist, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+        Fragment detailFragment = new GalleryDetailFragment();
+        Bundle args = new Bundle();
+        args.putString("param1", parent.getItemAtPosition(position).toString());
+        detailFragment.setArguments(args);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_content, detailFragment).commit();
+
+    }
+}
